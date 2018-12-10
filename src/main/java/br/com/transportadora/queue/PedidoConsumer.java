566e7ad25ba5146ca55b34f72a726337374c2efe
Tadeu Oliveira <tadeuandre@gmail.com>
2018-12-10 19:45:06 -0200

    Queue Implementation

---
+++ src/main/java/br/com/transportadora/queue/PedidoConsumer.java
@@ -0,0 +1,13 @@
+package br.com.transportadora.queue;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoConsumer {
+
+	@RabbitListener(queues = "${jsa.rabbitmq.queue}")
+	public void recievedMessage(String cep) {
+		System.out.println("Recieved Message: " + cep);
+	}
+}
